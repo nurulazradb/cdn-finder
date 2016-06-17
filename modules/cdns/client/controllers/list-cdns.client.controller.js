5dac03d1e7b36ee13a19d0e7fddb106f1d1bb04d
Nurulazrad Murad <nurulazrad@gmail.com>
2016-06-17 16:27:46 +0800

    Add search and filter for CDN

--- modules/cdns/client/controllers/list-cdns.client.controller.js
+++ modules/cdns/client/controllers/list-cdns.client.controller.js
@@ -5,11 +5,33 @@
     .module('cdns')
     .controller('CdnsListController', CdnsListController);
 
-  CdnsListController.$inject = ['CdnsService'];
+  CdnsListController.$inject = ['$scope', 'CdnsService'];
 
-  function CdnsListController(CdnsService) {
+  function CdnsListController($scope, CdnsService) {
     var vm = this;
 
     vm.cdns = CdnsService.query();
+
+    $scope.filterOptions = {
+      types: [
+        { name: 'Show All' },
+        { name: 'JavaScript' },
+        { name: 'StyleSheet' }
+      ]
+    }
+
+    $scope.filterItem = {
+      type: $scope.filterOptions.types[0]
+    }
+
+    $scope.customFilter = function(data) {
+      if (data.type === $scope.filterItem.type.name) {
+        return true;
+      } else if ($scope.filterItem.type.name === 'Show All') {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }());
